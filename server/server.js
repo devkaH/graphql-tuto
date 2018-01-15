ecf674000dd65fe99eac6858fca0a56a8c76a58b
devKaH <ben.hammadi.kaoula@gmail.com>
2018-01-15 14:45:55 +0100

    Mutations with React t4-2 done

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import cors from 'cors';
 const PORT = 4000;
 const server = express();
 
-server.use('*', cors({ origin: 'http://localhost:3000' }));
+server.use('*', cors({ origin: 'http://localhost:3001' }));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({
   schema
